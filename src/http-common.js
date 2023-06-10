b8b786c7226a577dd7131ce9c493ecf72752a81e
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-06-10 02:13:29 +0200

    https add

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function fetchData(){
 
     try {
-        const response = await axios.get('http://mycv-project.herokuapp.com/api/project/');
+        const response = await axios.get('https://mycv-project.herokuapp.com/api/project/');
         return response.data
     }catch (error){
         console.log(error);
@@ -15,7 +15,7 @@ export async function fetchData(){
 
 
 const http = axios.create({
-    baseURL: "http://mycv-project.herokuapp.com/api",
+    baseURL: "https://mycv-project.herokuapp.com/api",
     headers: {
       "Content-type": "application/json"
     }
